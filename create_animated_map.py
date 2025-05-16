17d018b6fa2cabddac4cea09caf553cd5577840d
Camille Labrousse <camille.m.labrousse@gmail.com>
2025-05-16 17:32:59 +0300

    edit

--- create_animated_map.py
+++ create_animated_map.py
@@ -63,7 +63,7 @@ def create_animation_from_gpx(gpx_file_path, output_html_path):
 
     TimestampedGeoJson(
         {'type': 'FeatureCollection', 'features': features},
-        period="PT2M", # Changed from PT1M to PT2M to cover more ground per step
+        period="PT1M", # Changed from PT1M to PT2M to cover more ground per step
         transition_time=30, # Changed from 50 to 30 for quicker transitions
         auto_play=True,
         loop=False,
